3af2a662c401dd2925c76f5e4fa93e2280598320
quencykb02 <quencycuccittini@hotmail.com>
2019-11-25 07:59:38 -0800

    portfolio

---
+++ Portofolio/js/main.js
@@ -0,0 +1,45 @@
+//  css
+
+var buttonList = document.getElementsByClassName('barIcon')
+var elemList = document.getElementsByClassName('myBar');
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    var i = 0;
+
+    while (i < elemList.length) {
+        buttonList[i].addEventListener('click', move);
+        i++;
+    }
+}
+
+
+
+function move() {
+    var i = 0;
+
+    var n = event.target.className.split(" ")[0];
+    var value = event.target.className.split(" ")[1];
+
+
+    console.log(n);
+
+    if (i == 0) {
+        i = 1;
+        var elem = elemList[n];
+        console.log(elem);
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= value) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+// html
